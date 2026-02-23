9940944945e3e26f27e1e31cfe40beb2ed07911e
Shubhang Darshan <shubhangdarshan@gmail.com>
2026-02-23 13:17:05 +0530

    Created for WEEK-2 (PRINTING OOPS USING ASCII)
    Used string concatenation and multiple print statements

---
+++ OOPSBannerPrint.java
@@ -0,0 +1,11 @@
+public class OOPSBannerPrint {
+	public static void main (String arg[]){
+		System.out.println("   ***   " + " " + "   ***   " + " " + "****** " + " " +  "   *****");
+		System.out.println(" **   ** " + " " + " **   ** " + " " + "*    **" + " " +  " **     ");
+		System.out.println("**     **" + " " + "**     **" + " " + "*     *" + " " +  "**      ");
+		System.out.println("**     **" + " " + "**     **" + " " + "****** " + " " +  "  ***** ");
+		System.out.println("**     **" + " " + "**     **" + " " + "*      " + " " +  "      **");
+		System.out.println(" **   ** " + " " + " **   ** " + " " + "*      " + " " +  "**    **");
+		System.out.println("   ***   " + " " + "   ***   " + " " + "*      " + " " +  " ****** ");
+	}
+}
