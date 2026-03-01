5a77a16835f15f95c0044d8f7924af2b7f5aec9f
Shubhang Darshan <shubhangdarshan@gmail.com>
2026-03-01 23:12:07 +0530

    Create UC3OOPSBannerPrint.java
    UC3 done

---
+++ UC3OOPSBannerPrint.java
@@ -0,0 +1,12 @@
+public class UC3OOPSBannerPrint{
+    public static void main(String[] args){
+        System.out.println(String.join("   ***    " ,  "   ***    " ,  "******  " ,   "   ***** "));
+		System.out.println(String.join(" **   **  " ,  " **   **  " ,  "*    ** " ,   " **      "));
+		System.out.println(String.join("**     ** " ,  "**     ** " ,  "*     * " ,   "**       "));
+		System.out.println(String.join("**     ** " ,  "**     ** " ,  "******  " ,   "  *****  "));
+		System.out.println(String.join("**     ** " ,  "**     ** " ,  "*       " ,   "      ** "));
+		System.out.println(String.join(" **   **  " ,  " **   **  " ,  "*       " ,   "**    ** "));
+		System.out.println(String.join("   ***    " ,  "   ***    " ,  "*       " ,   " ******  "));
+    }
+    
+}
